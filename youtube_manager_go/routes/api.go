7b8b5f62ab2a9a25ab7eec347a68a9851a390aab
s-shiki <37012360+s-shiki@users.noreply.github.com>
2019-11-23 09:45:22 +0900

    Created API

---
+++ youtube_manager_go/routes/api.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/s-shiki/youtube_api_server/youtube_manager_go/web/api"
+	"github.com/labstack/echo"
+)
+
+func Init(e *echo.Echo) {
+	g := e.Group("/api")
+	{
+		g.GET("/popular", api.FetchMostPopularVideos())
+	}
+}
