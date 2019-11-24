0d423a029063a6d9a715d4e7eba10fe25d91b9cc
s-shiki <37012360+s-shiki@users.noreply.github.com>
2019-11-24 11:18:40 +0900

    Added functions to obtain detailed info on frontend

--- youtube_manager_nuxt/nuxt.config.js
+++ youtube_manager_nuxt/nuxt.config.js
@@ -28,6 +28,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '~/plugins/vue_youtube',
   ],
   /*
   ** Nuxt.js dev-modules
--- youtube_manager_nuxt/store/index.js
+++ youtube_manager_nuxt/store/index.js
@@ -2,7 +2,8 @@ import { createRequestClient } from '~/store/request_client';
 
 export const state = () => ({
     items: [],
-        meta: {},
+    item: {},
+    meta: {},
 })
 
 export const actions = {
@@ -11,12 +12,24 @@ export const actions = {
         const res = await client.get(payload.uri, payload.params);
         commit('mutatePopularVideos', res);
     },
+    async findVideo({ commit }, payload) {
+        const client = createRequestClient(this.$axios);
+        const res = await client.get(payload.uri);
+        const params = {
+            ...res.VideoList,
+        }
+        commit('mutateVideo', params);
+    },
 }
 
 export const mutations = {
     mutatePopularVideos(state, payload) {
         state.items = payload.items ? state.items.concat(payload.items) : []
         state.meta = payload
+    },
+    mutateVideo(state, payload) {
+        const params = (payload.items && payload.items.length > 0) ? payload.items[0] : {}
+        state.item = params
     },
 }
 
@@ -26,5 +39,8 @@ export const getters = {
     },
     getMeta(state) {
         return state.meta
+    },
+    getVideo(state) {
+        return state.item
     },
 }
